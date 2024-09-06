19f348c84c71592bb5face81dbf1e3b3b9c3b889
Mrityunjay <mjguptacse@gmail.com>
2024-09-06 22:53:59 +0530

    Developed application schem

---
+++ Backend/models/application.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    applicant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Accepted", "Rejected"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Application = mongoose.model("Application", applicationSchema);
