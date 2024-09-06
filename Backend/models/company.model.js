e8bc1401c1a665a1412b155c939ae306cf213c9c
Mrityunjay <mjguptacse@gmail.com>
2024-09-06 22:48:32 +0530

    Company and  Job schema done

---
+++ Backend/models/company.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      name: required,
+    },
+    description: {
+      type: String,
+      name: required,
+    },
+    website: {
+      type: String,
+      name: required,
+    },
+    location: {
+      type: String,
+      name: required,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const company = mongoose.model("Company", companySchema);
--- Backend/models/job.model.js
+++ Backend/models/job.model.js
@@ -1,46 +1,49 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    requirements: {
+      type: String,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    jobType: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: Number,
+      requires: true,
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    application: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Application",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  requirements: {
-    type: String,
-  },
-  salary: {
-    type: Number,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  jobType: {
-    type: String,
-    required: true,
-  },
-  position: {
-    type: Number,
-    requires: true,
-  },
-  company: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Company",
-    required: true,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  application: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Application",
-  },
-});
+  { timestamps: true }
+);
 export const Job = mongoose.model("Job", jobSchema);
