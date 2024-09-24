11137a3abfc05125a7286f46cae0c9fcc668557f
Mrityunjay <mjguptacse@gmail.com>
2024-09-24 22:48:51 +0530

    Update Company in progress

--- Backend/routes/company.route.js
+++ Backend/routes/company.route.js
@@ -6,10 +6,11 @@ import {
   registerCompany,
   updateCompany,
 } from "../controllers/company.controllers.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 router.route("/register").post(isAutheticated, registerCompany);
 router.route("/get").get(isAutheticated, getCompany);
 router.route("/get/:id").get(isAutheticated, getCompanyById);
-router.route("/update/:id").put(isAutheticated, updateCompany);
+router.route("/update/:id").put(isAutheticated, singleUpload, updateCompany);
 export default router;
--- Frontend/src/components/admin/companySetup.jsx
+++ Frontend/src/components/admin/companySetup.jsx
@@ -1,7 +1,158 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Navbar from "../navbar";
+import { Button } from "../ui/button";
+import { ArrowLeft } from "lucide-react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { COMPANY_API_END_POINT } from "@/utils/constant";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
+import axios from "axios";
 const companySetup = () => {
-  return <div>companySetup</div>;
+  const [input, setInput] = useState({
+    name: "",
+    description: "",
+    website: "",
+    location: "",
+    file: null,
+  });
+  const [loading, setLoading] = useState(false);
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const params = useParams();
+  const navigate = useNavigate();
+  const companyId = params.id;
+  const changeFileHandler = (e) => {
+    const file = e.target.files?.[0];
+    setInput({ ...input, file });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", input.name);
+    formData.append("description", input.description);
+    formData.append("location", input.location);
+    formData.append("website", input.website);
+
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+
+    try {
+      setLoading(true);
+      const res = await axios.put(
+        `${COMPANY_API_END_POINT}/update/${companyId}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
+
+      if (res.data.succes) {
+        toast.success(res.data.message);
+        navigate("/admin/companies");
+      }
+    } catch (error) {
+      console.log(error);
+      // Error handling for displaying toast notifications
+      if (error.response) {
+        const errorData = error.response.data;
+        console.log(errorData);
+        if (Array.isArray(errorData.errors)) {
+          // Display each error message if errors is an array
+          errorData.errors.forEach((err) => {
+            toast.error(err.message);
+          });
+        } else if (errorData.message) {
+          toast.error(errorData.message);
+        } else {
+          // Fallback error message
+          toast.error("An unexpected error occurred");
+        }
+      } else if (error.request) {
+        // Handle no response received
+        toast.error("No response received from server");
+      } else {
+        // Handle error in setting up the request
+        toast.error(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-xl mx-auto my-10 border-black border p-4">
+        <form onSubmit={submitHandler}>
+          <div className="flex items-center gap-5 p-8 ">
+            <Button
+              variant="outline"
+              className="flex items-center gap-2 text-gray-500 font-semibold"
+            >
+              <ArrowLeft />
+              <span>Back</span>
+            </Button>
+            <h1 className="font-bold text-xl">Company Setup</h1>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col gap-3">
+              <Label>Company Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={input.name}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={input.description}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Location</Label>
+              <Input
+                type="text"
+                name="location"
+                value={input.location}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Website</Label>
+              <Input
+                type="text"
+                name="website"
+                value={input.website}
+                onChange={changeEventHandler}
+              />
+            </div>{" "}
+            <div className="flex flex-col gap-3">
+              <Label>Logo</Label>
+              <Input
+                type="file"
+                accept="image/*"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+
+          <Button className="w-full text-white bg-black mt-8" type="submit">
+            Update
+          </Button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default companySetup;
