d0991f5124ec9a2d42ee2fca06a780819e1271ab
Mrityunjay <mjguptacse@gmail.com>
2024-09-26 21:38:45 +0530

    updated admin job Table

--- Frontend/src/components/admin/adminJobsTable.jsx
+++ Frontend/src/components/admin/adminJobsTable.jsx
@@ -38,7 +38,6 @@ const adminJobsTable = () => {
   return (
     <div>
       <Table>
-        <TableCaption>A List of your recently Posted Jobs</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="text-center">Company Name</TableHead>
