95e75a4641aa042bfa61203700431f2565f292a1
Mrityunjay <mjguptacse@gmail.com>
2024-09-29 21:53:39 +0530

    category carousel search is working

--- Frontend/src/components/categorycarousel.jsx
+++ Frontend/src/components/categorycarousel.jsx
@@ -7,6 +7,9 @@ import {
   CarouselPrevious,
 } from "./ui/carousel";
 import { Button } from "./ui/button";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setSearchQuery } from "@/slices/jobSlice";
 
 const category = [
   "Frontend Developer",
@@ -18,6 +21,12 @@ const category = [
 ];
 
 const CategoryCarousel = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const searchJobHandler = (query) => {
+    dispatch(setSearchQuery(query));
+    navigate("/browse");
+  };
   return (
     <div className="animate-fadeInLeft">
       <Carousel className="w-full max-w-xl mx-auto my-28">
@@ -27,7 +36,11 @@ const CategoryCarousel = () => {
               key={index}
               className="md:basis-1/2 flex lg:basis-1/3   "
             >
-              <Button variant="outline" className="rounded-full font-semibold">
+              <Button
+                onClick={() => searchJobHandler(cat)}
+                variant="outline"
+                className="rounded-full font-semibold"
+              >
                 {cat}
               </Button>
             </CarouselItem>
