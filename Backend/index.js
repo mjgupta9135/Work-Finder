66deacec57787c56ebb5320127c692da46200d22
Mrityunjay <mjguptacse@gmail.com>
2024-09-30 10:22:54 +0530

    added all hosted url in server and frontend side

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://work-finder-ten.vercel.app/",
   credentials: true,
 };
 app.use(cors(corsOption));
