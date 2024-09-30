de6fed5455bc25cddaf56ed8faaea6f06e983af9
Mrityunjay <mjguptacse@gmail.com>
2024-09-30 11:56:15 +0530

    settings done for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -7,32 +7,32 @@ import connectDB from "./utils/db.js";
 import companyRouter from "./routes/company.route.js";
 import jobRouter from "./routes/job.routes.js";
 import applicationRouter from "./routes/application.route.js";
-
+import path from "path";
 dotenv.config({});
 const app = express();
 
-// Middleware
+const _dirname = path.resolve();
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-const corsOptions = {
-  origin: "https://work-finder-ten.vercel.app",
+const corsOption = {
+  origin: "https://job-hunt-nve6.onrender.com",
   credentials: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
 };
+app.use(cors(corsOption));
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests globally
-
-// APIs
+//apis
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
-// Start Server
+app.use(express.static(path.join(_dirname, "/Frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"));
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   connectDB();
