09cd63a7b1913c9c664840f177c23d5549bad23a
Mrityunjay <mjguptacse@gmail.com>
2024-09-30 10:30:39 +0530

    fixes cors error

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-  origin: "https://work-finder-ten.vercel.app/",
+  origin: "https://work-finder-ten.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOption));
