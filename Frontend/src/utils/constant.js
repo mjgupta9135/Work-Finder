47980db17eab10e3c53183d20b6b56cfb85154bf
Mrityunjay <mjguptacse@gmail.com>
2024-10-03 11:15:16 +0530

    api routes are modified

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,7 +1,8 @@
 export const USER_API_END_POINT =
-  "https://work-finder-m7mo.onrender.com/v1/user";
-export const JOB_API_END_POINT = "https://work-finder-m7mo.onrender.com/v1/job";
+  "https://work-finder-m7mo.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://work-finder-m7mo.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://work-finder-m7mo.onrender.com/v1/application";
+  "https://work-finder-m7mo.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://work-finder-m7mo.onrender.com/v1/company";
+  "https://work-finder-m7mo.onrender.com/v1/api/company";
